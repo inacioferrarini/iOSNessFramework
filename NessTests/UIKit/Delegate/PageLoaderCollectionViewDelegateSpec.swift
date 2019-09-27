a7d5d8155c57cc6b7dc0dc6c0ba8ef6206c36200
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-27 00:08:31 -0300

    Improves coverage

---
+++ NessTests/UIKit/Delegate/PageLoaderCollectionViewDelegateSpec.swift
@@ -0,0 +1,9 @@
+//
+//  PageLoaderCollectionViewDelegateSpec.swift
+//  NessTests
+//
+//  Created by José Inácio Athayde Ferrarini on 27/09/19.
+//  Copyright © 2019 José Inácio Athayde Ferrarini. All rights reserved.
+//
+
+import Foundation
---
+++ NessTests/UIKit/Delegate/TableViewBlockDelegateSpec.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewBlockDelegateSpec.swift
+//  NessTests
+//
+//  Created by José Inácio Athayde Ferrarini on 26/09/19.
+//  Copyright © 2019 José Inácio Athayde Ferrarini. All rights reserved.
+//
+
+import Foundation
